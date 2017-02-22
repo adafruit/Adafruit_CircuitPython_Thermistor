54d200f4c97fdc8bc3cc9e4f65da683ae1abfb65
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-02-22 11:31:01 +0100

    Initial thermistor driver. It works on a Circuit Playground express.

---
+++ README.rst
@@ -0,0 +1,53 @@
+
+Introduction
+============
+
+.. image:: https://readthedocs.org/projects/adafruit-circuitpython-thermistor/badge/?version=latest
+    :target: https://circuitpython.readthedocs.io/projects/thermistor/en/latest/
+    :alt: Documentation Status
+
+.. image :: https://badges.gitter.im/adafruit/circuitpython.svg
+    :target: https://gitter.im/adafruit/circuitpython?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge
+    :alt: Gitter
+
+TODO
+
+Dependencies
+=============
+This driver depends on:
+
+* `Adafruit CircuitPython <https://github.com/adafruit/circuitpython>`_
+
+Please ensure all dependencies are available on the CircuitPython filesystem.
+This is easily achieved by downloading
+`the Adafruit library and driver bundle <https://github.com/adafruit/Adafruit_CircuitPython_Bundle>`_.
+
+Usage Example
+=============
+
+The hardest part of using the driver is its initialization. Here is an example
+for the thermistor on the Circuit Playground and Circuit Playground Express. Its
+a 10k series resistor, 10k nominal resistance, 25 celsius nominal temperature and
+3950 B coefficient.
+
+.. code-block : python
+
+  import adafruit_thermistor
+  import board
+  thermistor = adafruit_thermistor.Thermistor(board.TEMPERATURE, 10000, 10000, 25, 3950)
+  print(thermistor.temperature)
+
+Contributing
+============
+
+Contributions are welcome! Please read our `Code of Conduct
+<https://github.com/adafruit/Adafruit_CircuitPython_thermistor/blob/master/CODE_OF_CONDUCT.md>`_
+before contributing to help this project stay welcoming.
+
+API Reference
+=============
+
+.. toctree::
+   :maxdepth: 2
+
+   api
---
+++ adafruit_thermistor.py
@@ -0,0 +1,76 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""
+`adafruit_thermistor` - Read temperature with a thermistor
+====================================================
+
+A thermistor is a resistor that varies with temperature. This driver takes the
+parameters of that resistor and its series resistor to determine the current
+temperature. To hook one up, connect an analog input pin to the connection
+between the resistor and the thermistor.
+
+* Author(s): Scott Shawcroft
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+* Adafruit `10K Precision Epoxy Thermistor - 3950 NTC <https://www.adafruit.com/products/372>`_  (Product ID: 372)
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the ESP8622 and M0-based boards: https://github.com/adafruit/circuitpython/releases
+
+**Notes:**
+
+#. Check the datasheet of your thermistor for the values.
+"""
+
+import math
+import nativeio
+
+class Thermistor:
+    """Thermistor driver"""
+
+    def __init__(self, pin, series_resistor, nominal_resistance, nominal_temperature, b_coefficient):
+        self.pin = nativeio.AnalogIn(pin)
+        self.series_resistor = series_resistor
+        self.nominal_resistance = nominal_resistance
+        self.nominal_temperature = nominal_temperature
+        self.b_coefficient = b_coefficient
+
+    @property
+    def temperature(self):
+        """The temperature of the thermistor in celsius"""
+        reading = self.pin.value / 64
+        reading = (1023 * self.series_resistor) / reading
+        reading -= self.series_resistor
+
+        steinhart = reading / self.nominal_resistance  # (R/Ro)
+        steinhart = math.log(steinhart)               # ln(R/Ro)
+        steinhart /= self.b_coefficient                # 1/B * ln(R/Ro)
+        steinhart += 1.0 / (self.nominal_temperature + 273.15)  # + (1/To)
+        steinhart = 1.0 / steinhart               # Invert
+        steinhart -= 273.15                       # convert to C
+
+        return steinhart
